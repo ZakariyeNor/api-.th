98947499b4f03f0885f0a393e0aee508b8ca979a
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-11-05 16:43:18 +0000

    dd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-//my key 
-const API_KEY = "zwF3qlgEkeiylVP4ltnxHwWRt6w";
-const API_URL = "https;//ci-jshint.herokuapp.com/api";
-
+const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
     const data = await response.json();
 
-    if(response.ok) {
+    if (response.ok) {
         console.log(data);
     };
+
 };
